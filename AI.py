cfe773f068f18fe80c19edef5b37c800aa41eee1
Mohammad Mahdi Saeedi <m.mahdisaeedi99@gamil.com>
2020-02-20 11:43:56 +0330

    update client

--- AI.py
+++ AI.py
@@ -3,6 +3,7 @@ import random
 
 from model import *
 import datetime
+from world import World
 
 
 class AI:
@@ -34,12 +35,12 @@ class AI:
 
 
     # this function is called in the beginning for deck picking and pre process
-    def pick(self, world):
+    def pick(self, world: World):
         self.f.write('player id: ' + str(world.get_me().player_id) + '\n')
         print("pick started!")
         world.get_cast_spell_by_id(id = 1.1)
 
-        # preprocess
+        # pre process
         map = world.get_map()
         self.rows = map.row_num
         self.cols = map.col_num
@@ -69,7 +70,7 @@ class AI:
             self.f.write(str(i.id))
 
     # it is called every turn for doing process during the game
-    def turn(self, world):
+    def turn(self, world: World):
         f = self.f
         f.write(f"turn started:{world.get_current_turn()}\n")
         print("turn started:", world.get_current_turn())
@@ -226,7 +227,7 @@ class AI:
     # it is called after the game ended and it does not affect the game.
     # using this function you can access the result of the game.
     # scores is a map from int to int which the key is player_id and value is player_score
-    def end(self, world, scores):
+    def end(self, world: World, scores):
         print("end started!")
         print("My score:", scores[world.get_me().player_id])
 
