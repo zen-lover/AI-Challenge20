6224410969147fc298851d81da531d5f07420096
Sahar Shayegan <SaharOmidiShayegan@gmail.com>
2020-03-03 18:44:47 +0330

    vase defa fasele ro 8 kardm

--- AIC20-Client-Python-master/AI.py
+++ AIC20-Client-Python-master/AI.py
@@ -585,7 +585,7 @@ class AI:
         elif self.bayadbfrstm == False:
             print('''hala bayad masir entekhab koni''')
             t = self.get_closest_enemy_path_and_dist(world, world.get_me())
-            if t[1] <= 7:
+            if t[1] <= 8:
                 print(f'ye masire jadid baraye doshman peyda kardam: {t[0].id}')
                 print('''in yani bayad beshe masire jadidet''')
                 self.masir = t[0]
